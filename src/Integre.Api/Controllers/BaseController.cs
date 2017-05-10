e49a1c65ecf4f35c8a747fc958930eb4f8166673
Herlan Rodrigues Siqueira <herlan.siqueira@inventsoftware.com.br>
2017-05-10 14:46:08 -0300

    Atualizada Api

--- src/Integre.Api/Controllers/BaseController.cs
+++ src/Integre.Api/Controllers/BaseController.cs
@@ -1,11 +1,52 @@
-﻿using System;
+﻿using FluentValidator;
+using Integre.Infra.Transactions;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Integre.Api.Controllers
 {
-    public class BaseController
+    public class BaseController : Controller
     {
+        private readonly IUow _uow;
+
+        public BaseController(IUow uow)
+        {
+            _uow = uow;
+        }
+
+        public async Task<IActionResult> Response(object result, IEnumerable<Notification> notifications)
+        {
+            if (!notifications.Any())
+            {
+                try
+                {
+                    _uow.Commit();
+                    return Ok(new
+                    {
+                        success = true,
+                        data = result
+                    });
+                }
+                catch
+                {
+                    // Logar o erro (Elmah)
+                    return BadRequest(new
+                    {
+                        success = false,
+                        errors = new[] { "Ocorreu uma falha interna no servidor." }
+                    });
+                }
+            }
+            else
+            {
+                return BadRequest(new
+                {
+                    success = false,
+                    errors = notifications
+                });
+            }
+        }
     }
 }
