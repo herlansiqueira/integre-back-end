23b34a0ce7c446fd18cc2eaeb31a731c7be262e2
Herlan Rodrigues Siqueira <herlan.siqueira@inventsoftware.com.br>
2017-05-10 14:36:47 -0300

    Implementada Api

---
+++ src/Integre.Api/Startup.cs
@@ -0,0 +1,103 @@
+﻿using Integre.Api.Security;
+using Integre.Domain.Commands.Handlers;
+using Integre.Domain.Repositories;
+using Integre.Domain.Services;
+using Integre.Infra.Contexts;
+using Integre.Infra.Repositories;
+using Integre.Infra.Services;
+using Integre.Infra.Transactions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
+namespace Integre.Api
+{
+    public class Startup
+    {
+
+        private const string ISSUER = "c1f51f42";
+        private const string AUDIENCE = "c6bbbb645024";
+        private const string SECRET_KEY = "c1f51f42-5727-4d15-b787-c6bbbb645024";
+
+        private readonly SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SECRET_KEY));
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                                 .RequireAuthenticatedUser()
+                                 .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+            services.AddCors();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("User", policy => policy.RequireClaim("Integre", "User"));
+                options.AddPolicy("Admin", policy => policy.RequireClaim("Integre", "Admin"));
+            });
+
+            services.Configure<TokenOptions>(options =>
+            {
+                options.Issuer = ISSUER;
+                options.Audience = AUDIENCE;
+                options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
+            });
+
+            services.AddScoped<DataContext, DataContext>();
+            services.AddTransient<IUow, Uow>();
+            services.AddTransient<ICollaboratorRepository, CollaboratorRepository>();
+            services.AddTransient<IEmailService, EmailService>();
+            services.AddTransient<CollaboratorCommandHandler, CollaboratorCommandHandler>();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = ISSUER,
+
+                ValidateAudience = true,
+                ValidAudience = AUDIENCE,
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signingKey,
+
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+
+                ClockSkew = TimeSpan.Zero
+            };
+
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                TokenValidationParameters = tokenValidationParameters
+            });
+
+            app.UseCors(x =>
+            {
+                x.AllowAnyHeader();
+                x.AllowAnyMethod();
+                x.AllowAnyOrigin();
+            });
+
+            app.UseMvc();
+        }
+    }
+}
