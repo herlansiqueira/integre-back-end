4864bdf5c7d28fc9ae1a168a13cad5ebc3802d7f
Herlan Rodrigues Siqueira <herlan.siqueira@inventsoftware.com.br>
2017-05-11 08:16:43 -0300

    Alterada string connection

--- src/Integre.Infra/Migrations/Configuration.cs
+++ src/Integre.Infra/Migrations/Configuration.cs
@@ -1,9 +1,6 @@
 namespace Integre.Infra.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Integre.Infra.Contexts.DataContext>
     {
--- src/Integre.Shared/Runtime.cs
+++ src/Integre.Shared/Runtime.cs
@@ -2,6 +2,6 @@
 {
     public static class Runtime
     {
-        public static string ConnectionString = "";
+        public static string ConnectionString = "Server=tcp:integre-server.database.windows.net,1433;Initial Catalog=integre;Persist Security Info=False;User ID=integre;Password=!devel58;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
